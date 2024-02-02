e259c6fbb765c70d94a23a87311e4471a52394b1
nilayganatra447@gmail.com <nilayganatra447@gmail.com>
2024-02-02 14:04:31 -0500

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GitNilayFirstName
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GitNilayFirstName
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
